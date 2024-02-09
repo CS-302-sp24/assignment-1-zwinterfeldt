e2215573259027a148aabfb052027e8b5a89012e
zwinterfeldt <zwinterfeldt25@gmail.com>
2024-02-09 17:25:34 -0600

    need to fix

--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -10,7 +10,7 @@ class Philosopher extends Thread {
 
   // TODO might not need thinkingTime
   // private int thinkingTime;
-  private int eatingTime;
+  private int maxEatingTime;
 
   // TODO Might not need this one either
   // private int rightLeft;
@@ -18,14 +18,14 @@ class Philosopher extends Thread {
   // Constructor
   // int eatingTime, int rightLeft, int numCycles,
 
-  public Philosopher(Chopstick left, Chopstick right, int thinkingTime) {
+  public Philosopher(Chopstick left, Chopstick right, int thinkingTime, int maxEatingTime) {
     this.left = left;
     this.right = right;
 
     // set instance variables
     // this.numCycles = numCycles;
     // this.thinkingTime = thinkingTime;
-    this.eatingTime = eatingTime;
+    this.maxEatingTime = maxEatingTime;
     // this.rightLeft = rightLeft;
 
     random = new Random();
@@ -43,12 +43,19 @@ class Philosopher extends Thread {
 
         // Think for a time between 0 and 1000
         Thread.sleep(random.nextInt(1000)); // Think for a while
+        System.out.println("Philosopher " + this + "wants the left chopstick.");
         synchronized (left) { // Grab left chopstick
           synchronized (right) { // Grab right chopstick
+            System.out.println("Philosopher " + this + "wants the right chopstick.");
             Thread.sleep(random.nextInt(1000)); // Eat for a while
-            ++eatingTime;
-            if (thinkCount % 10 == 0)
-              System.out.println("Philosopher " + this + " has eaten for " + eatingTime + " ms");
+
+            // Redo this entire part
+            while (thinkCount < maxEatingTime) {
+              ++maxEatingTime;
+              System.out.println("Philosopher " + this + " has eaten for " + maxEatingTime);
+            }
+            // redo ^^
+
           }
         }
       }
