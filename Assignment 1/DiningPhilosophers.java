68893cfd582e6c7fee75db4fa73bca4e332f2d34
zwinterfeldt <zwinterfeldt25@gmail.com>
2024-02-09 17:10:34 -0600

    initial changes

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -3,13 +3,18 @@ public class DiningPhilosophers {
   public static void main(String[] args) throws InterruptedException {
     Philosopher[] philosophers = new Philosopher[5];
     Chopstick[] chopsticks = new Chopstick[5];
-    
+
+    // Create 5 new chopsticks
     for (int i = 0; i < 5; ++i)
       chopsticks[i] = new Chopstick(i);
+
+    // Creare 5 new Philosophers
     for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5], 12);
       philosophers[i].start();
     }
+
+    // Waits for Philosophers (threads) to die
     for (int i = 0; i < 5; ++i)
       philosophers[i].join();
   }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -5,24 +5,54 @@ class Philosopher extends Thread {
   private Random random;
   private int thinkCount;
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
+  // Add our instance variables from command line arguments
+  // private int numCycles;
+
+  // TODO might not need thinkingTime
+  // private int thinkingTime;
+  private int eatingTime;
+
+  // TODO Might not need this one either
+  // private int rightLeft;
+
+  // Constructor
+  // int eatingTime, int rightLeft, int numCycles,
+
+  public Philosopher(Chopstick left, Chopstick right, int thinkingTime) {
+    this.left = left;
+    this.right = right;
+
+    // set instance variables
+    // this.numCycles = numCycles;
+    // this.thinkingTime = thinkingTime;
+    this.eatingTime = eatingTime;
+    // this.rightLeft = rightLeft;
+
     random = new Random();
   }
 
   public void run() {
     try {
-      while(true) {
+      while (true) {
         ++thinkCount;
+        // Every 10 seconds print out how many times a philosopher has thought
+
+        // TODO Potentially remove this and replace it with time
         if (thinkCount % 10 == 0)
           System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
+
+        // Think for a time between 0 and 1000
+        Thread.sleep(random.nextInt(1000)); // Think for a while
+        synchronized (left) { // Grab left chopstick
+          synchronized (right) { // Grab right chopstick
             Thread.sleep(random.nextInt(1000)); // Eat for a while
+            ++eatingTime;
+            if (thinkCount % 10 == 0)
+              System.out.println("Philosopher " + this + " has eaten for " + eatingTime + " ms");
           }
         }
       }
-    } catch(InterruptedException e) {}
+    } catch (InterruptedException e) {
+    }
   }
 }
