897438beceb81dc7dad402681973d002778418d4
zwinterfeldt <zwinterfeldt25@gmail.com>
2024-02-11 15:26:00 -0600

    new

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,12 +1,27 @@
+import java.io.*;
+
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
 
+    try{
     int numPhilosophers = Integer.parseInt(args[0]);
     int thinkingTime = Integer.parseInt(args[1]);
     int maxEatingTime = Integer.parseInt(args[2]);
     int numCycles = Integer.parseInt(args[3]);
     int rightLeft = Integer.parseInt(args[4]);
+
+    System.out.println(System.getProperty("user.dir"));
+
+    FileWriter fileWriter = new FileWriter("C:/Users/zwint//IdeaProjects/assignment-1-zwinterfeldt/test3.txt");
+    PrintWriter printWriter = new PrintWriter(fileWriter);
+
+    System.setOut(new PrintStream(new OutputStream() {
+      @Override
+      public void write(int b) throws IOException {
+        printWriter.write(b);
+      }
+    }));
 
     Philosopher[] philosophers = new Philosopher[numPhilosophers];
     Chopstick[] chopsticks = new Chopstick[numPhilosophers];
@@ -24,6 +39,13 @@ public class DiningPhilosophers {
     // Waits for Philosophers (threads) to die
     for (int i = 0; i < numPhilosophers; ++i) {
       philosophers[i].join();
+    }
+
+    printWriter.close();
+    fileWriter.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -1,6 +1,9 @@
 import java.util.Random;
+import java.io.*;
 
 class Philosopher extends Thread {
+  private FileWriter fileWriter;
+  private PrintWriter printWriter;
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
@@ -24,6 +27,13 @@ class Philosopher extends Thread {
     // this.rightLeft = rightLeft;
     this.id = id+1;
     this.rightLeft = rightLeft;
+    try{
+      fileWriter = new FileWriter("test3.txt");
+      printWriter = new PrintWriter(fileWriter);
+    }
+    catch (IOException e){
+      e.printStackTrace();
+    }
     /*
     if((this.id)%2==0){
 
@@ -38,6 +48,7 @@ class Philosopher extends Thread {
 
   public void run() {
     try {
+      System.out.println(System.getProperty("user.dir"));
       int currCycles = 0;
       while (true) {
         while (currCycles<=this.numCycles){
@@ -50,23 +61,23 @@ class Philosopher extends Thread {
         // All philosophers are right-handed
           if (rightLeft==0){
             if (thinkCount % 10 == 0) {
-              System.out.println("Philosopher " + this.id + " has thought " + thinkCount + " times");
+              printWriter.println("Philosopher " + this.id + " has thought " + thinkCount + " times");
             }
             // Think for a time between 0 and 1000
             Thread.sleep(random.nextInt(1000)); // Think for a while
-            System.out.println("Philosopher " + this.id + " wants the left chopstick.");
+            printWriter.println("Philosopher " + this.id + " wants the left chopstick.");
             synchronized (left) { // Grab left chopstick
-              System.out.println("Philosopher " + this.id + " has the left chopstick.");
-              System.out.println("Philosopher " + this.id + " wants the right chopstick.");
+              printWriter.println("Philosopher " + this.id + " has the left chopstick.");
+              printWriter.println("Philosopher " + this.id + " wants the right chopstick.");
               synchronized (right) { // Grab right chopstick
-                System.out.println("Philosopher " + this.id + " has the right chopstick.");
+                printWriter.println("Philosopher " + this.id + " has the right chopstick.");
                 Thread.sleep(random.nextInt(1000)); // Eat for a while
 
                 // Redo this entire part
                 while (eatCount < maxEatingTime) {
                   ++eatCount;
                 }
-                System.out.println("Philosopher " + this.id + " has eaten for " + eatCount);
+                printWriter.println("Philosopher " + this.id + " has eaten for " + eatCount);
                 // Reset the eat count
                 this.eatCount = 0;
                 // redo ^^
@@ -76,38 +87,38 @@ class Philosopher extends Thread {
           // Even philosophers are right-handed, odd are left-handed (hypothetically should produce a deadlock)
           if(rightLeft!=0) {
             if (thinkCount % 10 == 0) {
-              System.out.println("Philosopher " + this.id + " has thought " + thinkCount + " times");
+              printWriter.println("Philosopher " + this.id + " has thought " + thinkCount + " times");
             }
             // Think for a time between 0 and 1000
             Thread.sleep(random.nextInt(1000)); // Think for a while
             if(this.id % 2 != 0) {
-              System.out.println("Philosopher " + this.id + " wants the left chopstick.");
+              printWriter.println("Philosopher " + this.id + " wants the left chopstick.");
               // Grab the left chopstick first
               synchronized (left) {
-                System.out.println("Philosopher " + this.id + " has the left chopstick.");
-                System.out.println("Philosopher " + this.id + " wants the right chopstick.");
+                printWriter.println("Philosopher " + this.id + " has the left chopstick.");
+                printWriter.println("Philosopher " + this.id + " wants the right chopstick.");
                 synchronized (right) {
-                  System.out.println("Philosopher " + this.id + " has the right chopstick.");
+                  printWriter.println("Philosopher " + this.id + " has the right chopstick.");
                   while (eatCount < maxEatingTime) {
                     ++eatCount;
                   }
-                  System.out.println("Philosopher " + this.id + " has eaten for " + eatCount);
+                  printWriter.println("Philosopher " + this.id + " has eaten for " + eatCount);
                   // Reset the eat count
                   this.eatCount = 0;
                 }
               }
             }
             else {
-              System.out.println("Philosopher" + this.id + "wants the right chopstick");
+              printWriter.println("Philosopher " + this.id + " wants the right chopstick");
               synchronized (left){
-                System.out.println("Philosopher" + this.id + "has the right chopstick");
-                System.out.println("Philosopher" + this.id + "wants the left chopstick");
+                printWriter.println("Philosopher " + this.id + " has the right chopstick");
+                printWriter.println("Philosopher " + this.id + " wants the left chopstick");
                 synchronized (right){
-                  System.out.println("Philosopher" + this.id + "has the left chopstick");
+                  printWriter.println("Philosopher " + this.id + " has the left chopstick");
                   while (eatCount < maxEatingTime) {
                     ++eatCount;
                   }
-                  System.out.println("Philosopher " + this.id + " has eaten for " + eatCount);
+                  printWriter.println("Philosopher " + this.id + " has eaten for " + eatCount);
                   // Reset the eat count
                   this.eatCount = 0;
                 }
@@ -119,7 +130,11 @@ class Philosopher extends Thread {
         }
         break;
       }
+      printWriter.close();
+      //fileWriter.close();
     } catch (InterruptedException e) {
+      e.printStackTrace();
+      printWriter = new PrintWriter(System.out);
     }
   }
 }
